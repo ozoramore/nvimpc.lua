f115e112c407521d6668b38e18d003ad01d450c2
mori_oh <dev@example.com>
2025-02-06 15:46:30 +0900

    libuvを利用してフルスクラッチすることにする

--- lua/nvimpc/init.lua
+++ lua/nvimpc/init.lua
@@ -1,8 +1,24 @@
-local mpd = require('nvimpc/lua-mpd/mpd')
+local uv = vim.uv
 local M = {}
 
-function M.setup(opts)
-	return mpd.new(opts)
+M.connection = nil
+
+M.config = {
+	host = 'localhost',
+	port = 6600,
+	password = nil
+}
+
+local function connect(opts)
+	local client = uv.new_tcp()
+	client:connect( M.config.host, M.config.port, function(err) print(err) end )
+	uv.run()
 end
+
+M.setup = function(opts)
+	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+end
+
+
 
 return M
