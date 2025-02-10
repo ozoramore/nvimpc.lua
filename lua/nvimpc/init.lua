2458edc73793082819345655e4ecd21e031ecba6
mori_oh <dev@example.com>
2025-02-10 14:30:55 +0900

    コードのフォーマットをする

--- lua/nvimpc/init.lua
+++ lua/nvimpc/init.lua
@@ -1,19 +1,3 @@
-local uv = vim.uv
-local M = {}
-
-M.config = {
-	host = "localhost",
-	addr = nil,
-	port = 6600,
-	password = nil,
-	isv6 = false,
-}
-
-M.result = {}
-M.connection = false
-
-local resultbuf = ""
-
 local function split(str, ts)
 	ts = ts or "\n"
 	local t = {}
@@ -25,31 +9,17 @@ local function split(str, ts)
 	return t
 end
 
-local connect = function(host, port, data)
-	local client = uv.new_tcp()
-	assert(client, "new tcp fail")
-	local writebuf = data .. "\nclose\n"
-	resultbuf = ""
-	M.connection = true
-	client:send_buffer_size()
-	client:connect(host, port, function(status)
-		assert(not status, status)
-		client:write(writebuf, function(status_write)
-			assert(not status_write, status_write)
-			client:read_start(function(status_read, readbuf)
-				assert(not status_read, status_read)
-				if readbuf then
-					resultbuf = resultbuf .. readbuf
-				else
-					client:shutdown(function()
-						M.connection = false
-						client:close()
-					end)
-				end
-			end)
-		end)
-	end)
-end
+local uv = vim.uv
+
+local M = {}
+
+M.config = {
+	host = "localhost",
+	addr = nil,
+	port = 6600,
+	password = nil,
+	isv6 = false,
+}
 
 M.setup = function(opts)
 	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
@@ -60,12 +30,57 @@ M.setup = function(opts)
 	M.config.addr = M.config.addr or uv.getaddrinfo(M.config.host, nil, { family = family_type })[1].addr
 end
 
-M.command = function(opts)
-	connect(M.config.addr, M.config.port, opts)
-	vim.wait(1000, function()
-		return not M.connection
-	end)
-	M.result = split(resultbuf,nil)
+M.result = {}
+
+M.command = function(data)
+	local is_connect = false
+	local writebuf = data .. "\nclose\n"
+	local resultbuf = ""
+	local client = uv.new_tcp()
+	assert(client, "new tcp fail")
+	client:send_buffer_size()
+
+	--
+	-- callback関数の定義
+	--
+
+	local on_shutdown = function()
+		is_connect = false
+		client:close()
+	end
+
+	local on_read_start = function(status, buf)
+		assert(not status, status)
+		if buf then
+			resultbuf = resultbuf .. buf
+		else
+			client:shutdown(on_shutdown)
+		end
+	end
+
+	local on_write = function(status)
+		assert(not status, status)
+		client:read_start(on_read_start)
+	end
+
+	local on_connect = function(status)
+		assert(not status, status)
+		is_connect = true
+		client:write(writebuf, on_write)
+	end
+
+	--
+	-- callback関数の定義 end
+	--
+
+	client:connect(M.config.addr, M.config.port, on_connect)
+
+	local waiting = function()
+		return not is_connect
+	end
+	vim.wait(1000, waiting)
+	M.result = split(resultbuf, nil)
 end
+
 
 return M
